af24e02cbb7633adf292ac13b82048de387e6d92
Hamidalion <fearofwar@mail.ru>
2020-10-06 21:18:45 +0300

    feat: add nuget

---
+++ src/ProperNutrition.DAL/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProperNutrition.DAL.Entities
+{
+    /// <summary>
+    /// User Identities
+    /// </summary>
+    public class User
+    {
+    }
+}
